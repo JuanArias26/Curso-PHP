d6df333cd345e2c84687995b00764dba58dbd695
Juan <jsnino286@soy.sena.edu.co>
2023-09-27 09:12:19 -0500

    arreglo3

--- ejercicio6.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $Edad=32;
-    $Nombre="Juan";
-    $Altura=1.4;
-    $Programador=null;
-    echo " El nombree es ".$Nombre." la edad es ".$Edad." su altura es ".$Altura." el puesto ".$Programador;
-    ?>
